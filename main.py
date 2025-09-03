f93b4c60d7826b8336f00a7f5c074a5b767ece2b
xvhjs2 <tramtram4400@gmail.com>
2025-09-02 23:03:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ huddyevidence = "79ee349b6511e2000af8a32fb8a6974e"
 blacklisted_names = ['huddy', 'hanzala', 'hanza', 'hamzi', 
                      'hamza', 'hanzi', 'mama666', 'tungdynasty', "daisysdestruction", 'nanda', 'northkorea', 'NotSoFantastic', 'necro', 'uncannyrapist', 'uncannyrpiper', '亡靈開膛手', 'lolodin'] 
 
+blacklisted_names2 = ['neku', 'henry', 'henery', 'franzer', 'xv', 'kale', 'beau', 'viviun', 'bajel', 'day', 'adam', 'nana', 'kimi']
+
+obvjudah = ['dick', 'tits', 'pussy', 'judah', 'ass', 'boobs', 'cum', 'fuck', 'nigger', 'neku', 'henry', 'henery', 'franzer', 'xv', 'kale', 'beau', 'viviun', 'bajel', 'day', 'adam', 'nana', 'kimi']
+
 config = "data/servers.json"
 if not os.path.exists(config):
     with open(config, 'w') as f:
@@ -42,6 +46,7 @@ with open('huddyids.txt', 'r') as f:
 async def on_ready():
     print(f'logged in as {blud.user}')
 
+safe_ids = ['1408332135873384450', '1091167315678212216', '972883349163098214', '1160646380214296716', '763882154173923368', '309437317813239809', '705536958990123079', '861291608968265759', '780287531840700418', '1313368024673423401', '1310784364375834714', '1157181396884914176', '1389358473313255517', '732676316704276501']
 
 @blud.event
 async def on_member_join(member):
@@ -78,6 +83,16 @@ async def on_member_join(member):
             except:
                 print('failed to ban necrophillia guy :(')
 
+        elif any(obvjud in member.display_name.lower() for obvjud in blacklisted_names2) and any(obvjudah in member.display_name.lower() for obvjud in blacklisted_names2) and not str(member.id) in safe_ids:
+            try:
+                await member.send("kill yourself judah")
+            except:
+                print('failed to send message')
+            try:
+                await member.ban(reason="potential judah alt account")
+            except:
+                print('failed to ban necrophillia guy :(')
+        
         elif str(member.id) in blacklisted_ids:
             try:
                 await member.send('ur def a huddy alt so ur banned ')
@@ -110,7 +125,22 @@ async def banhuddy(ctx):
     print(f'huddy detector is {status}')
     await ctx.reply(embed=embed)
 
-        
+@bot.command()
+async def banjudah(ctx):
+    guild = ctx.guild
+    judahids = ['1328567772116029542', '1293000690117120020', '867433511316357151', '1412620373861863434', '815986860521685033', '1394485081673826425', '958441194524913725', '1148077609457033288']
+    embed = discord.Embed(title='Huddy Detector', description="Massbanning Judah's accounts")
+    await ctx.reply(embed=embed)
+    for id in blacklisted_ids:
+        try:
+            huddy = await ctx.bot.fetch_user(id)
+            await ctx.guild.ban(huddy, reason='judah kill yourself')
+            print(f'banned {huddy}')
+        except:
+            print('failed to ban account')
+    embed2 = discord.Embed(title='Huddy Detector', description="✅ Successfully banned Judah's accounts")
+    await ctx.reply(embed=embed2)
+
 @blud.command(name='banhuddy', help="Bans most of Huddy's past accounts")
 async def masshuddyban(ctx):
     embed = discord.Embed(title='Huddy Detector', description="Massbanning Huddy's accounts")
@@ -125,4 +155,4 @@ async def masshuddyban(ctx):
     embed2 = discord.Embed(title='Huddy Detector', description="✅ Successfully banned Huddy's accounts")
     await ctx.reply(embed=embed2)
 
-blud.run('bot token')
+blud.run('enter your bot token here')
